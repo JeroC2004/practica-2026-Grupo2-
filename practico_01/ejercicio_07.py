2c246ed1dd27cecb6c632aa6f08c2e5f87652bd5
EliasMoramarco <emoramarco@frro.utn.edu.ar>
2026-04-07 15:07:10 -0300

    ejercicio 3/7/11

--- practico_01/ejercicio_07.py
+++ practico_01/ejercicio_07.py
@@ -8,8 +8,7 @@ def es_palindromo(palabra: str) -> bool:
     Restricción: No utilizar bucles - Usar Slices de listas.
     Referencia: https://docs.python.org/3/tutorial/introduction.html#lists
     """
-    pass # Completar
-
+    return palabra == palabra[::-1]
 
 # NO MODIFICAR - INICIO
 assert not es_palindromo("amor")
@@ -28,7 +27,12 @@ def mitad(palabra: str) -> str:
     Restricción: No utilizar bucles - Usar Slices de listas.
     Referencia: https://docs.python.org/3/tutorial/introduction.html#lists
     """
-    pass # Completar
+    if len(palabra) % 2 == 0:
+        mitadPalabra = len(palabra) // 2
+    else:
+        mitadPalabra = len(palabra) // 2 + 1
+
+    return palabra[:mitadPalabra]
 
 
 # NO MODIFICAR - INICIO
